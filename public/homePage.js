9467f3a835ad369aa3158d0d5ded71aac0767018
Marina Osborne <marina08088@mail.ru>
2020-09-18 18:53:01 +0300

    диплом 1809

--- public/homePage.js
+++ public/homePage.js
@@ -1,10 +1,101 @@
 'use strict'
 const logoutButton = new LogoutButton;
 
-logoutButton.action = ApiConnector.logout(response => console.log(response))
-// logoutButton.action = ApiConnector.logout(response => {
-//     console.log(response);
-// //   if (response.success === true) location.reload(); 
-        
-    // }));
+logoutButton.action = f => ApiConnector.logout(response => {
+    if (response.success === true) {
+        location.reload(); 
+        } 
+   });
+
+     
+ApiConnector.current(response => {
+    if (response.success === true) {
+        ProfileWidget.showProfile(response.data)
+        }
+    });
+
+
+const ratesBoard = new RatesBoard;
+
+let timerId = setInterval(ApiConnector.getStocks(response => {
+
+  if (response.success === true) {
+
+     ratesBoard.clearTable();
+     ratesBoard.fillTable(response.data)
+  }
+
+}), 120000);    
+
+
+const moneyManager = new MoneyManager;
+
+moneyManager.addMoneyCallback = data => ApiConnector.addMoney(data, response => {
+    if (response.success === true) {
+
+        moneyManager.addMoneyForm.reset = ProfileWidget.showProfile(response.data); 
+        moneyManager.setMessage(response.success, "Операция прошла успешно")
+
+        } else {
+            moneyManager.setMessage(response.success, "Введите корректные данные")
+        }
+   });
+
    
+
+moneyManager.conversionMoneyCallback = data => ApiConnector.convertMoney(data, response => {
+    if (response.success === true) {
+
+        moneyManager.conversionMoneyForm.reset = ProfileWidget.showProfile(response.data); 
+        moneyManager.setMessage(response.success, "Операция прошла успешно")
+        
+        } else {
+            moneyManager.setMessage(response.success, "Введите корректные данные")
+        }
+   });
+
+moneyManager.sendMoneyCallback = data => ApiConnector.transferMoney(data, response => {
+    if (response.success === true) {
+
+        moneyManager.sendMoneyForm.reset = ProfileWidget.showProfile(response.data); 
+        moneyManager.setMessage(response.success, "Операция прошла успешно")
+        
+        } else {
+            moneyManager.setMessage(response.success, "Введите корректные данные")
+        }
+   });
+
+  const favoritesWidget = new FavoritesWidget;
+   
+  ApiConnector.getFavorites(response => {
+    if (response.success === true) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        }
+    });
+
+    favoritesWidget.addUserCallback = data => ApiConnector.addUserToFavorites(data, response => {
+        if (response.success === true) {
+    
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(data);
+            moneyManager.updateUsersList(data);
+            favoritesWidget.setMessage(response.success, "Операция прошла успешно")
+            } else {
+                moneyManager.setMessage(response.success, "Введите корректные данные")
+            }
+        });
+    
+        
+    favoritesWidget.removeUserCallback = data => ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success === true) {
+    
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(data);
+            moneyManager.updateUsersList(data);
+            favoritesWidget.setMessage(response.success, "Операция прошла успешно")
+            } else {
+                moneyManager.setMessage(response.success, "Введите корректные данные")
+            }
+        });        
